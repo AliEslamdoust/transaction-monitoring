73db2eb508f994ca6f31bb1cb025ea3a04895abc
root <root@DESKTOP-6FP3T7P.localdomain>
2025-12-02 10:51:25 +0330

    Installed whitenoise as middleware to serve static files through daphne

--- transaction_monitoring/transaction_monitoring/settings.py
+++ transaction_monitoring/transaction_monitoring/settings.py
@@ -1,3 +1,4 @@
+# run daphne server with this command: daphne -b 0.0.0.0 -p 8001 transaction_monitoring.asgi:application
 """
 Django settings for transaction_monitoring project.
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
